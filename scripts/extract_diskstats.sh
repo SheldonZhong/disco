a83cc94a961874a46186445baaf1bfd3a2e92d8b
Wenshao Zhong <wzhong20@uic.edu>
2024-09-09 17:53:19 -0500

    clean up iotrace tools, add extract scripts

---
+++ scripts/extract_diskstats.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ "$#" -ne 2 ]; then
+  echo "<filename> <disk>"
+  exit
+fi
+
+base=${1%.log}
+
+drive=$(basename $2)
+
+write_end=$(grep -w ${drive} ${base}-end-smart.log | awk '{print $8}')
+write_start=$(grep -w ${drive} ${base}-start-smart.log | awk '{print $8}')
+read_end=$(grep -w ${drive} ${base}-end-smart.log | awk '{print $4}')
+read_start=$(grep -w ${drive} ${base}-start-smart.log | awk '{print $4}')
+
+write_diff=$((${write_end} - ${write_start}))
+read_diff=$((${read_end} - ${read_start}))
+
+swrite_end=$(grep -w ${drive} ${base}-end-smart.log | awk '{print $10}')
+swrite_start=$(grep -w ${drive} ${base}-start-smart.log | awk '{print $10}')
+sread_end=$(grep -w ${drive} ${base}-end-smart.log | awk '{print $6}')
+sread_start=$(grep -w ${drive} ${base}-start-smart.log | awk '{print $6}')
+
+swrite_diff=$((${write_end} - ${write_start}))
+sread_diff=$((${read_end} - ${read_start}))
+
+echo total write: $write_diff
+echo total read: $read_diff
+
+echo total sector write: $swrite_diff
+echo total sector read: $sread_diff
---
+++ scripts/extract_smartctl.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ "$#" -ne 1 ]; then
+  echo "Needs a file name of the main log file"
+  exit
+fi
+
+base=${1%.log}
+
+write_end=$(grep "Logical Sectors Written" ${base}-end-smart.log | awk '{print $4}')
+write_start=$(grep "Logical Sectors Written" ${base}-start-smart.log | awk '{print $4}')
+read_end=$(grep "Logical Sectors Read" ${base}-end-smart.log | awk '{print $4}')
+read_start=$(grep "Logical Sectors Read" ${base}-start-smart.log | awk '{print $4}')
+
+if [ -z "$write_end" ]; then
+  write_end=$(grep "Blocks received from initiator" ${base}-end-smart.log | awk '{print $6}')
+fi
+
+if [ -z "$write_start" ]; then
+  write_start=$(grep "Blocks received from initiator" ${base}-start-smart.log | awk '{print $6}')
+fi
+
+if [ -z "$read_end" ]; then
+  read_end=$(grep "Blocks sent to initiator" ${base}-end-smart.log | awk '{print $6}')
+fi
+
+if [ -z "$read_start" ]; then
+  read_start=$(grep "Blocks sent to initiator" ${base}-start-smart.log | awk '{print $6}')
+fi
+
+write_diff=$((${write_end} - ${write_start}))
+read_diff=$((${read_end} - ${read_start}))
+
+tot_write=$(python -c "print(($write_diff if $write_diff >= 0 else $write_diff + 2 ** 32))")
+tot_read=$(python -c "print(($read_diff if $read_diff >= 0 else $read_diff + 2 ** 32))")
+
+tot_write_bytes=$(python -c "print($tot_write * 512)")
+tot_read_bytes=$(python -c "print($tot_read * 512)")
+
+echo write I/O: $tot_write
+echo read I/O: $tot_read
+
+echo write bytes: $tot_write_bytes
+echo read bytes: $tot_read_bytes
