4083a51090354cc248d0942e0dcc37853b9140b7
Wenshao Zhong <wzhong20@uic.edu>
2024-09-10 17:06:13 -0500

    fix extract_dbtest.sh

--- scripts/extract_dbtest.sh
+++ scripts/extract_dbtest.sh
@@ -17,19 +17,29 @@ else
   rgen_col=6
 fi
 
+data_line=$(grep "total" ${base}-err.log)
+
 rgen_arg_col=$(($rgen_col + 1))
 
-if grep -q "uniform" ${base}-err.log; then
-  pass_col=$(($rgen_col + 4))
-else
-  pass_col=$(($rgen_col + 5))
-fi
+rgen=$(echo ${data_line} | awk -v rgen_arg_col="$rgen_arg_col" '{print $rgen_arg_col}')
+
+case $rgen in
+  shuffle|uniform)
+    pass_col=$(($rgen_col + 4))
+    ;;
+  unizipf)
+    pass_col=$(($rgen_col + 5))
+    ;;
+  *)
+    echo "rgen ${rgen} num args not defined"
+    exit 0
+    ;;
+esac
 
 klen_col=$(($pass_col + 8))
 vlen_col=$(($pass_col + 9))
 num_ops_col=$(($pass_col + 11))
 
-data_line=$(grep "total" ${base}-err.log)
 
 klen=$(echo ${data_line} | awk -v klen_col="$klen_col" '{print $klen_col}')
 vlen=$(echo ${data_line} | awk -v vlen_col="$vlen_col" '{print $vlen_col}')
@@ -37,9 +47,9 @@ num_ops=$(echo ${data_line} | awk -v num_ops_col="$num_ops_col" '{print $num_ops
 nsec=$(echo ${data_line} | awk '{print $2}')
 sysname=$(echo ${data_line} | awk '{print $4}')
 
-nkv=$(echo ${data_line} | awk '{print $19}')
+nkv_col=$(($rgen_col + 3))
 
-rgen=$(echo ${data_line} | awk -v rgen_arg_col="$rgen_arg_col" '{print $rgen_arg_col}')
+nkv=$(echo ${data_line} | awk -v nkv_col="$nkv_col" '{print $nkv_col}')
 
 ops=$(echo "scale=5; $num_ops / $nsec" | bc)
 
